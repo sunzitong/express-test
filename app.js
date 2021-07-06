96f9cc6d5af66f6308e5f83bd6dbfe191a0c16c0
sunzitong <642626517@qq.com>
2021-07-06 23:47:00 +0800

    save

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { default: axios } = require('axios');
 const express = require('express');
 const app = express();
 const post = require('./routes/post')
@@ -5,7 +6,18 @@ const port = 3389;
 
 app.use(express.json());
 
-app.use('/post', post)
+app.post('/submit', (req, res) => {
+  console.log('req', req.body);
+  axios({
+    method: 'post',
+    url: 'https://lincoln.iooo.net/media_test/core.php/api/apply/media',
+    data: req.body
+  }).then((response) => {
+    res.send(response.data);
+  }).catch((err) => {
+    res.status(err.response.status).send(err.response.data);
+  })
+})
 
 app.listen(port, () => {
   console.log(`Express serve listening at http://localhost:${port}`)
--- routes/post.js
+++ routes/post.js
@@ -1,21 +1,40 @@
 const express = require('express');
 const request = require('request');
+const axios = require('axios');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  request('http://m.test109.iguanyu.com/app-server/api/v1/main/clientConfig', (error, response, body) => {
-    console.log('error:', error);
-    console.log('response.statusCode:', response.statusCode);
-    console.log('body:', body);
-    // if (!error && response.statusCode === 200) {
-    //   var data = JSON.parse(body);
-    //   res.send(data);
-    // } else {
-    //   res.send('{error:404}');
-    // }
-    res.send(JSON.parse(body))
+  axios({
+    method: 'get',
+    // url: 'https://lincoln.iooo.net/media_test/core.php/api/apply/media',
+    url: 'http://m.test109.iguanyu.com/app-server/api/v1/main/clientConfig1',
+    // data: {
+    //   USERNAME: 'cjzz',
+    //   PASSWORD: window.md5(`123456${Date.now()}`),
+    //   TIMESTAMP: Date.now(),
+    //   SOURCE: 'cjzz',
+    //   CONTENT: JSON.stringify(payload),
+    // },
+  }).then((a,b,c) => {
+    console.log('a',a);
+    console.log('b',b);
+    console.log('c',c);
+  }).catch((e) => {
+    console.log('e' ,e);
   });
+  // request('http://m.test109.iguanyu.com/app-server/api/v1/main/clientConfig', (error, response, body) => {
+  //   console.log('error:', error);
+  //   console.log('response.statusCode:', response.statusCode);
+  //   console.log('body:', body);
+  //   // if (!error && response.statusCode === 200) {
+  //   //   var data = JSON.parse(body);
+  //   //   res.send(data);
+  //   // } else {
+  //   //   res.send('{error:404}');
+  //   // }
+  //   res.send(JSON.parse(body))
+  // });
 })
 
 router.post('/', (req, res) => {
